63520a789459d5eb2930009a051f4990c390050d
Bennett Gale <bennett.p.gale@gmail.com>
2018-08-21 19:15:58 +1000

    Implemented BFGS optimisation

--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@
 #include <cmath>
 #include <lbfgs.h>
 
+
+//#define DEBUG
+
 struct Molecule {
-	unsigned length;
+	int n_atoms;
 	std::vector<double> bond_angles;
 
-	explicit Molecule(unsigned length)
+	explicit Molecule(int n_atoms)
 	{
-		this->length = length;
-		bond_angles.resize(length - 2);
+		this->n_atoms = n_atoms;
+		bond_angles.resize((unsigned long)n_atoms - 1);
+	}
+
+	explicit  Molecule(double* angles, int n_angles)
+	{
+		n_atoms = n_angles + 1;
+		bond_angles.resize((unsigned long)n_angles);
+		std::copy(angles, angles + n_angles, this->bond_angles.begin());
 	}
 };
 
@@ -19,10 +29,13 @@ struct Cartesian {
     double x, y;
 };
 
+// --------------------- Function Prototypes -------------------------- //
+double lj_potential(const Molecule& s);
+
 /**
  * This function is completely random and does not consider knots
  */
-Molecule rand_mol(unsigned len)
+Molecule rand_mol(int len)
 {
 	Molecule s(len);
 	std::random_device rd;
@@ -30,20 +43,21 @@ Molecule rand_mol(unsigned len)
 	std::uniform_real_distribution<double> uniform(-M_PI, M_PI);
 	for (auto& i : s.bond_angles)
 		i = uniform(gen);
+	s.bond_angles[0] = 0;
 	return s;
 }
 
-void print_csv(Molecule& s)
+template <typename Iterable>
+void print_csv(const Iterable& angles, const double energy, int n_angles)
 {
-	for (auto i = 0; i < s.bond_angles.size(); i++) {
-		std::cout << s.bond_angles[i];
-		if (i != s.bond_angles.size() - 1)
-			std::cout << ",";
+	for (auto i = 0; i < n_angles; i++) {
+		std::cout << angles[i] << ",";
 	}
-	std::cout << std::endl;
+	std::cout << energy << std::endl;
 }
 
 
+// Note: Ensure that cart has at least s.length elements
 void angle_to_cart(const Molecule& s, std::vector<Cartesian> &cart)
 {
 	cart[0] = {0, 0};
@@ -59,13 +73,13 @@ void angle_to_cart(const Molecule& s, std::vector<Cartesian> &cart)
 /**
  * Calculate the Lennard-Jones potential energy of the given system s
  */
-double lj_potential(Molecule& s)
+double lj_potential(const Molecule& s)
 {
-	std::vector<Cartesian> cart(s.length);
+	std::vector<Cartesian> cart((unsigned long)s.n_atoms);
 	angle_to_cart(s, cart);
     double v = 0;
-    for (int i = 0; i < s.length - 1; i++) {
-        for (int j = i + 1; j < s.length; j++) {
+    for (int i = 0; i < s.n_atoms - 1; i++) {
+        for (int j = i + 1; j < s.n_atoms; j++) {
         	double delta_x = cart[j].x - cart[i].x;
         	double delta_y = cart[j].y - cart[i].y;
             v += 1 / pow(pow(delta_x, 2) + pow(delta_y, 2), 6)
@@ -75,12 +89,100 @@ double lj_potential(Molecule& s)
     return v;
 }
 
+// Length of grads must be equal to s.length-2 and be able to contain doubles
+template<typename Iterable>
+void lj_gradient(const Molecule& s, Iterable grads)
+{
+	std::vector<Cartesian> cart((unsigned long)s.n_atoms);
+	angle_to_cart(s, cart);
+	for (int m = 0; m < s.bond_angles.size(); m++) {
+		double dv = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = m+1; j < s.n_atoms; j++) {
+				double dx = cart[i].x - cart[j].x;
+				double dy = cart[i].y - cart[j].y;
+				double r_sqrd = pow(dx, 2) + pow(dy, 2);
+				double res = (1.0/pow(r_sqrd, 7) - 1.0/pow(r_sqrd, 4))
+						* ( dx * (cart[m].y - cart[j].y)
+						+ dy * (cart[j].x - cart[m].x) );
+				dv += res;
+			}
+		}
+		grads[m] = 12 * dv;
+		//std::cout << "grads[" << m << "]: " << grads[m] << std::endl;
+	}
+}
+
+// The callback interface to provide objective function and gradient evaluations
+// for liblbfgs
+lbfgsfloatval_t lbfgs_evaluate(
+		void* instance,
+		const lbfgsfloatval_t* x,
+		lbfgsfloatval_t* g,
+		const int n,
+		const lbfgsfloatval_t step
+		)
+{
+	Molecule s((double*)x, n);
+	lj_gradient(s, g);
+	return lj_potential(s);
+}
+
+int lbfgs_progress(
+		void* instance,
+		const lbfgsfloatval_t* x,
+		const lbfgsfloatval_t* g,
+		const lbfgsfloatval_t fx,
+		const lbfgsfloatval_t xnorm,
+		const lbfgsfloatval_t gnorm,
+		const lbfgsfloatval_t step,
+		int n,
+		int k,
+		int ls
+		)
+{
+#ifdef DEBUG
+	printf("Iteration %d:\n", k);
+	printf("  fx = %f, x[0] = %f, x[1] = %f\n", fx, x[0], x[1]);
+	printf("  xnorm = %f, gnorm = %f, step = %f\n", xnorm, gnorm, step);
+	printf("\n");
+#endif
+	return k >= 99;
+}
+
 int main()
 {
-	Molecule m(4);
-	std::vector<double> angles = {0, M_PI / 4.0, -M_PI / 2.0};
-	m.bond_angles = angles;
-	print_csv(m);
-	std::cout << lj_potential(m) << std::endl;
-	return 0;
+//	Molecule m(5);
+//	std::vector<double> angles = {0, M_PI/6, M_PI/6, M_PI/6};
+//	m.bond_angles = angles;
+	for (int i = 0; i < 10; i++) {
+		Molecule m = rand_mol(5);
+		double optimal_result = 0;
+		lbfgsfloatval_t* x = lbfgs_malloc((int) m.bond_angles.size());
+		std::copy(m.bond_angles.begin(), m.bond_angles.end(), x);
+		lbfgs_parameter_t param;
+		lbfgs_parameter_init(&param);
+		int status = lbfgs(
+				(int) m.bond_angles.size(),
+				x,
+				&optimal_result,
+				lbfgs_evaluate,
+				lbfgs_progress,
+				nullptr,
+				&param
+		);
+		print_csv(x, optimal_result, (int) m.bond_angles.size());
+#ifdef DEBUG
+		if (status == LBFGS_ALREADY_MINIMIZED)
+			std::cout << "LBFGS_ALREADY_MINIMIZED" << std::endl;
+		else if (status == LBFGSERR_ROUNDING_ERROR)
+			std::cout << "LBFGSERR_ROUNDING_ERROR" << std::endl;
+		else if (status == LBFGSERR_INCORRECT_TMINMAX)
+			std::cout << "LBFGSERR_INCORRECT_TMINMAX" << std::endl;
+		else
+			std::cout << status << std::endl;
+#endif
+		lbfgs_free(x);
+		}
+		return 0;
 }
--- plot.py
+++ plot.py
@@ -8,15 +8,21 @@ from math import cos, sin
 def plot(angle_chain, fig_name):
     plt.figure()
     plt.axis('equal')
+    angle_chain = angle_chain.strip()
     chain = [float(x) for x in angle_chain.split(',')]
     x = [0]
     y = [0]
     cumulative = 0
-    for alpha in chain:
-        cumulative += alpha
+    for i in range(0, len(chain)):
+        if i == len(chain) - 1:
+            chart_title = "Energy: " + str(chain[i])
+            print(chart_title)
+            continue
+        cumulative += chain[i]
         x.append(x[-1] + cos(cumulative))
         y.append(y[-1] + sin(cumulative))
     plt.plot(x, y, 'o-')
+    plt.title(chart_title)
     plt.savefig("./documentation/figure " + fig_name + ".png")
     plt.close()
 
