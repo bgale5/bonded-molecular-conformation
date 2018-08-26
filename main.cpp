bc0a4428543b2c77e60a76a9cf970a5a2cd64dd9
Bennett Gale <bennett.p.gale@gmail.com>
2018-08-26 11:33:43 +1000

    Implemented single-point crossover function

--- main.cpp
+++ main.cpp
@@ -13,20 +13,21 @@
 struct Molecule {
 	int n_atoms;
 	double fitness;
-	std::vector<double> bond_angles;
+	std::vector<double> angles;
+	// TODO: Store the cartesian coordinates so that they aren't recomputed.
 
 	explicit Molecule(int n_atoms)
 	{
 		this->n_atoms = n_atoms;
-		bond_angles.resize((unsigned long) n_atoms-1);
+		angles.resize((unsigned long) n_atoms-1);
 		fitness = std::numeric_limits<double>::max();
 	}
 
 	explicit Molecule(double* angles, int n_angles)
 	{
 		n_atoms = n_angles+1;
-		bond_angles.resize((unsigned long) n_angles);
-		std::copy(angles, angles+n_angles, this->bond_angles.begin());
+		this->angles.resize((unsigned long) n_angles);
+		std::copy(angles, angles+n_angles, this->angles.begin());
 		fitness = std::numeric_limits<double>::max();
 	}
 };
@@ -35,7 +36,8 @@ struct Cartesian {
 	double x, y;
 };
 
-typedef void (* genetic_op_ptr)(Molecule&);
+typedef void (* crossover_ptr)(const Molecule*, const Molecule*, Molecule*, Molecule*);
+typedef void (* mutation_ptr)(Molecule*);
 
 // --------------------- Function Prototypes -------------------------- //
 double lj_potential(const Molecule& s);
@@ -44,16 +46,15 @@ double lj_potential(const Molecule& s);
 /**
  * This function is completely random and does not consider knots
  */
-Molecule rand_mol(int len)
+void randomise_mol(Molecule* s)
 {
-	Molecule s(len);
 	std::random_device rd;
 	std::mt19937 gen(rd()); // New Mersenne-Twister generator seeded with rd
 	std::uniform_real_distribution<double> uniform(-M_PI, M_PI);
-	for (auto& i : s.bond_angles)
+	for (auto& i : s->angles)
 		i = uniform(gen);
-	s.bond_angles[0] = 0;
-	return s;
+	s->angles[0] = 0;
+	s->fitness = std::numeric_limits<double>::max();
 }
 
 template<typename Indexible>
@@ -70,8 +71,8 @@ void angle_to_cart(const Molecule& s, std::vector<Cartesian>& cart)
 {
 	cart[0] = {0, 0};
 	double phi = 0;
-	for (int i = 0; i<s.bond_angles.size(); i++) {
-		phi += s.bond_angles[i];
+	for (int i = 0; i<s.angles.size(); i++) {
+		phi += s.angles[i];
 		double x = cart[i].x+cos(phi);
 		double y = cart[i].y+sin(phi);
 		cart[i+1] = {x, y};
@@ -104,7 +105,7 @@ void lj_gradient(const Molecule& s, Indexible gradients)
 {
 	std::vector<Cartesian> cart((unsigned long) s.n_atoms);
 	angle_to_cart(s, cart);
-	for (int m = 0; m<s.bond_angles.size(); m++) {
+	for (int m = 0; m<s.angles.size(); m++) {
 		double dv = 0;
 		for (int i = 0; i<m; i++) {
 			for (int j = m+1; j<s.n_atoms; j++) {
@@ -158,73 +159,73 @@ int lbfgs_progress(
 	return k>=99;
 }
 
-void single_crossover(Molecule& s)
+void single_crossover(const Molecule* p1, const Molecule* p2, Molecule* c1, Molecule* c2)
 {
-	return;
+	//int cp = rand() % (int)p1->angles.size();
+	int cp = 2;
+	std::copy(p1->angles.begin(), p1->angles.begin()+cp, c1->angles.begin());
+	std::copy(p2->angles.begin()+cp, p2->angles.end(), c1->angles.begin()+cp);
+	std::copy(p2->angles.begin(), p2->angles.begin()+cp, c2->angles.begin());
+	std::copy(p1->angles.begin()+cp, p1->angles.end(), c2->angles.begin()+cp);
 }
 
-void double_crossover(Molecule& s)
+void single_mutation(Molecule* s)
 {
-	return;
+	unsigned long n = s->angles.size();
+	double old_fitness = s->fitness;
+	std::random_device rd;
+	std::mt19937 gen(rd()); // New Mersenne-Twister generator seeded with rd
+	std::uniform_real_distribution<double> uniform(-M_PI, M_PI);
+	do {
+		s->angles[rand()%n] = uniform(gen);
+	} while (old_fitness < 0 && s->fitness > 0); // If it makes a knot, try again.
 }
 
-void single_mutation(Molecule& s)
-{
-	return;
-}
-
-void n_mutation(Molecule& s)
-{
-	return;
-}
-
-void sa_mutation(Molecule& s)
-{
-	return;
-}
-
+// Create 2 parents, if either is better than the parent, replace the parent
+// with it. Constant pop size.
 Molecule ga(
-		Molecule** population,
-		int pop_size,
-		const genetic_op_ptr* crossovers,
-		const genetic_op_ptr* mutations,
-		int n_crossovers,
-		int n_mutations,
-		int max_generations
+		std::vector<Molecule*>& population,
+		const std::vector<crossover_ptr>& crossovers,
+		const std::vector<mutation_ptr>& mutations,
+		int max_gen
 )
 {
-	for (int gen = 0; gen<max_generations; gen++) {
-		gen;
+	int n_pop = (int)population.size();
+	int n_atoms = population[0]->n_atoms;
+	auto child1 = new Molecule(n_atoms);
+	auto child2 = new Molecule(n_atoms);
+
+	for (int gen = 0; gen < max_gen; gen++)
+	{
+		for (auto& m : population) {
+			print_csv(m->angles, lj_potential(*m), (int)m->angles.size());
+		}
+		crossovers[0](population[0], population[1], child1, child2);
+		print_csv(child1->angles, lj_potential(*child1), (int)child1->angles.size());
+		print_csv(child2->angles, lj_potential(*child2), (int)child2->angles.size());
 	}
-	return *(population[0]);
+	delete child1;
+	delete child2;
+	return *population[0];
 }
 
 int main()
 {
 	srand((unsigned int) time(nullptr));
-	const int n_cros = 2, n_mut = 3;
-	genetic_op_ptr crossovers[n_cros] = {single_crossover, double_crossover};
-	genetic_op_ptr mutations[n_mut] = {single_mutation, n_mutation, sa_mutation};
-	int n_pop = 50;
-	int n_atoms = 5;
-	int max_generations = 100;
-	Molecule* population[n_pop];
+	std::vector<crossover_ptr> crossovers = {single_crossover};
+	std::vector<mutation_ptr> mutations = {single_mutation};
+	int n_pop = 2;
+	int n_atoms = 6;
+	int max_generations = 1;
+	std::vector<Molecule*> population((unsigned long)n_pop);
 	for (auto& m : population) {
 		m = new Molecule(n_atoms);
+		randomise_mol(m);
 	}
 
 	// Run the genetic algorithm
-	Molecule fittest = ga(
-			population,
-			n_pop,
-			crossovers,
-			mutations,
-			n_cros,
-			n_mut,
-			max_generations
-	);
-
-	print_csv(fittest.bond_angles, fittest.fitness, (int) fittest.bond_angles.size());
+	Molecule fittest = ga(population, crossovers, mutations, max_generations);
+	//print_csv(fittest.angles, fittest.fitness, (int)fittest.angles.size());
 
 	// cleanup
 	for (auto& m : population) {
